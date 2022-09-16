24480c9f05767f41ce58ca0dd03ad37c1ebd504a
omidheidari88 <54186037+omidheidari88@users.noreply.github.com>
2022-09-16 20:38:52 +0000

    Deploying to gh-pages from @ omidheidari88/portfolio@23fa1c17d3b48b7cef276ad5ed388c3fb1fd5f24 🚀

---
+++ _next/static/chunks/d64684d8-b8843f34df5350cb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(a,b,c){c.d(b,{kzR:function(){return e}});var d=c(8357);function e(a){return(0,d.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M19 9l-7 7-7-7"}}]})(a)}}}])
