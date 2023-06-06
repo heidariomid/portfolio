5256717936f9041796733c1ba1ce08a1c608f665
Omid <54186037+heidariomid@users.noreply.github.com>
2023-06-06 17:33:07 +0330

    edit about me section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
 				isDark ? 'bg-zinc-900' : 'bg-gray-200'
 			}  `}
 		>
-			<motion.div className=' max-w-[1240px] mx-auto md:grid grid-cols-3 gap-8  '>
+			<motion.div className=' max-w-[1240px] mx-auto md:grid grid-cols-3 gap-8   '>
 				<motion.div className='col-span-2 '>
 					<motion.h2
 						initial={{opacity: 0, x: -400}}
@@ -23,46 +23,25 @@ const About = () => {
 							duration: 1.2,
 							ease: 'easeInOut',
 						}}
-						className={`tracking-widest 	${isDark ? 'text-white' : 'text-zinc-700'} px-10 md:px-0  uppercase `}
+						className={`tracking-widest mt-10 md:mt-0 	${isDark ? 'text-white' : 'text-zinc-700'} px-10 md:px-0  uppercase `}
 					>
 						About Me
 					</motion.h2>
 					<p className={` ${isDark ? 'text-zinc-200' : 'text-zinc-600'} md:text-2xl md:py-4  text-justify m-10 md:m-0  md:leading-loose  pr-4 `}>
-						Software developer with advanced skills in <span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>ReactJS</span>
-						, <span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Next.js</span>,{' '}
+						Software developer with expertise in front-end technologies, including{' '}
+						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>ReactJS</span>,{' '}
+						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Next.js</span>,{' '}
+						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>TypeScript</span>,{' '}
 						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Redux</span>,{' '}
 						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>GraphQL</span>, and{' '}
-						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Tailwind CSS</span>. While my focus is on the front-end,
-						I have a good understanding of back-end technologies such as{' '}
-						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Node.js</span>,{' '}
-						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>TypeScript</span>, and{' '}
-						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Design Patterns</span>. While I have not yet had the
-						opportunity to work for a company, I have dedicated myself to advancing my skills through self-learning in academies like{' '}
-						<a
-							className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}
-							href='https://7learn.com/'
-							target={'_blank'}
-							rel='noreferrer'
-						>
-							7Learn
-						</a>{' '}
-						and{' '}
-						<a
-							className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}
-							href='https://udemy.com/'
-							target={'_blank'}
-							rel='noreferrer'
-						>
-							Udemy
-						</a>{' '}
-						and have completed many advanced personal projects. I prefer working on the front end because it allows me to see the finished product
-						and the design process. While I enjoy the flexibility of remote work, I am also excited about the opportunity to work collaboratively as
-						part of a dynamic and fast-paced team. My goal is to leverage my skills and experience to deliver exceptional results in an environment
-						that values innovation, teamwork, and professional growth.
+						<span className={` ${isDark ? 'text-white' : 'text-zinc-800'} font-semibold `}>Tailwind CSS</span>. Skilled in developing intuitive and
+						visually appealing user interfaces, my aim is to deliver clean and structured code that will be easy to maintain in the future and build
+						for you scalable web applications. I am continuously dedicated to learning and staying up-to-date with the latest industry trends in
+						order to deliver the best possible service and improve my skills as a developer.
 					</p>
 				</motion.div>
 				<motion.div
-					className={`bg-clip-content bg-cover md:p-[15px] m-[45px]  ${
+					className={`hidden bg-clip-content bg-cover md:p-[15px] m-[45px]  ${
 						isDark ? 'border-zinc-700' : 'border-gray-200'
 					} md:m-0 md:border-4 ease-in-out duration-1000`}
 				>
